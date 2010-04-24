ffa6209020b99acf9ee3191606c815b00ba89ace
Eugene <bofh@redwerk.com>
2010-04-24 22:20:33 +0300

    Added signin code for LJ

--- LiveJournal/Auth.hs
+++ LiveJournal/Auth.hs
@@ -1,11 +1,56 @@
-module LiveJournal.Auth where 
+module LiveJournal.Auth (
+    Session(..),
+    login
+)
+where 
 
-import Network.Curl
+import Maybe
 
-data Session = Anonymous | Authenticated { auth_challange, auth_response :: String }
+import LiveJournal.Transport
+import Data.ByteString.Char8 as BStr
+import Data.ByteString.Lazy.Char8 as BStrL
+import Data.Digest.Pure.MD5
+import Prelude as P
 
-login :: String -> String -> IO (  )
-login username password = do
-    curl <- initialize
-    response <- do_curl curl "http://www.livejournal.com/interface/flat" method_POST
-    putStrLn $ respBody response
+data Session = Anonymous | Authenticated { auth_challenge, auth_response :: BStr.ByteString }
+
+instance Show Session where
+    show Anonymous = "Anonymous session"
+    show (Authenticated auth_challenge auth_response) = BStr.unpack auth_challenge ++ " :: " ++ BStr.unpack auth_response
+
+login :: String -> String -> IO ( Maybe Session )
+login username password = 
+    prepareChallenge password >>= login'
+    where
+        login' Nothing = return Nothing
+        login' (Just (chal, auth_response)) = do
+            response <- runRequest [makePair1 modeStr "login", 
+                                    makePair "user" username, 
+                                    makePair "auth_method" "challenge", 
+                                    makePair2 "auth_challenge" chal, 
+                                    makePair2 "auth_response" auth_response]
+            return $ findPair "success" response >>= createSession chal auth_response
+        createSession chal auth_response status  | status == statusOK = Just (Authenticated chal auth_response)
+                                                 | otherwise          = Nothing
+            
+
+modeStr = BStr.pack "mode"
+statusOK = BStr.pack "OK"
+
+prepareChallenge :: String -> IO (Maybe (BStr.ByteString, BStr.ByteString))
+prepareChallenge password = do
+    response <- runRequest [Pair modeStr getChallengeStr]
+    return . fmap (result) $ findPair "challenge" response
+    where
+        getChallengeStr = BStr.pack "getchallenge"
+        md5Pass = BStrL.pack . show . md5 $ BStrL.pack password -- have no idea how to force MD5Digest to be converted to lazy bytestring
+        repack = BStrL.fromChunks . (:[])
+        hashcode chal = md5 $ BStrL.concat [chal, md5Pass]
+        result chal = (chal, BStr.pack . show . hashcode . repack $ chal )
+    
+
+findPair :: String -> [Pair] -> Maybe BStr.ByteString
+findPair _ [] = Nothing
+findPair pName val = listToMaybe . P.map value . P.filter ( ( == pName') . name ) $ val
+    where
+        pName' = BStr.pack pName
--- LiveJournal/Transport.hs
+++ LiveJournal/Transport.hs
@@ -1,11 +1,36 @@
-module LiveJournal.Transport where
+module LiveJournal.Transport(
+    Pair(..),
+    makePair,
+    makePair1,
+    makePair2,
+    runRequest
+)
+where
 
+import Prelude as P
 import Network.Curl
+import Data.ByteString.Char8 as BStr
 
-data Pair = Pair { name, value :: String }
+data Pair = Pair { name, value :: ByteString }
+
+makePair :: String -> String -> Pair
+makePair strName strValue = Pair strName' strValue'
+    where
+        strName' = BStr.pack strName
+        strValue' = BStr.pack strValue
+
+makePair1 :: ByteString -> String -> Pair
+makePair1 strName strValue = Pair strName strValue'
+    where
+        strValue' = BStr.pack strValue
+
+makePair2 :: String -> ByteString -> Pair
+makePair2 strName strValue = Pair strName' strValue
+    where
+        strName' = BStr.pack strName
 
 instance Show Pair where
-    show (Pair name' value') = name' ++ "=" ++ value'
+    show (Pair name' value') = BStr.unpack name' ++ "=" ++ BStr.unpack value'
 
 runRequest :: [Pair] -> IO ( [Pair] )
 runRequest input = do
@@ -13,15 +38,15 @@ runRequest input = do
     fmap ( parseResponse . extractResponse ) $ do_curl_ curl "http://www.livejournal.com/interface/flat" curlOptions
     where
         curlOptions = joinPairs input:method_POST
-        joinPairs = CurlPostFields . map joinPair
+        joinPairs = CurlPostFields . P.map joinPair
         joinPair = show  -- may be some sort of escaping?
 
-extractResponse :: CurlResponse_ [(String,String)] String -> String
+extractResponse :: CurlResponse_ [(String,String)] ByteString -> ByteString
 extractResponse = respBody
 
-parseResponse :: String -> [Pair]
+parseResponse :: ByteString -> [Pair]
 parseResponse = buildPairs . clearEmpty
     where
-        clearEmpty = dropWhile ( == "" ) . lines
+        clearEmpty = P.dropWhile ( == BStr.empty ) . BStr.lines
         buildPairs (name:value:pairs) = Pair name value:buildPairs pairs
         buildPairs _ = []
