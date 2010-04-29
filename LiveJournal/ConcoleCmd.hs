712f3ef046153c8ce8b52c7ecee86c004acd748e
Eugene <bofh@redwerk.com>
2010-04-29 18:52:00 +0300

    Implemented parsing of ban_list results. Untested.

--- LiveJournal/ConcoleCmd.hs
+++ LiveJournal/ConcoleCmd.hs
@@ -1,10 +1,12 @@
 module ConsoleCmd where
 
+import Data.ByteString.Char8 as BStr
 import LiveJournal.Transport
 import LiveJournal.Common
 import LiveJournal.Error
 import Data.URLEncoded
-import Control.Monad.Error
+import Text.Regex.Posix
+import Prelude as P
 
 ban_set_from :: Session -> String -> String -> String -> IO ( Result () )
 ban_set_from Anonymous _ _ _ = return $ Right AuthRequired
@@ -20,12 +22,16 @@ ban_set_from session owner target comm = do
 ban_set :: Session -> String -> String -> IO ( Result () )
 ban_set session username target = ban_set_from session username target ""
 
-ban_list :: Session -> String -> String -> IO (Result [String])
-ban_list session username target = do
+ban_list_from :: Session -> String -> String -> IO (Result [String])
+ban_list_from Anonymous _ _ = return $ Right AuthRequired
+ban_list_from session username target = do
     makeLJCall session [makePair "mode" "ban_list",
                         makePair "user" username,
                         makePair "command" cmdString] buildUserList
     where
         cmdString | target == "" = "ban_list"
                   | otherwise    = "ban_list from " ++ target
-        buildUserList _ = Left []
+        buildUserList = Left . P.map ( BStr.unpack . value ) . P.filter ( (=~ "cmd_line_\\d+") . name ) 
+
+ban_list :: Session -> String -> IO (Result [String])
+ban_list session username = ban_list_from session username ""
