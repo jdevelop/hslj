efc22a1ed3671d2b44585b90adb0653dcf262a0d
Eugene <bofh@redwerk.com>
2010-05-08 20:48:56 +0300

    Completely implemented posting of an entry.

--- LiveJournal/Post/Event.hs
+++ LiveJournal/Post/Event.hs
@@ -1,10 +1,10 @@
 module LiveJournal.Post.Event where
 
-import Data.ByteString.UTF8 as BStr
 import Data.Word
 import Data.Maybe
 import Data.Array
 import LiveJournal.Pair
+import Data.DateTime
 
 data LineEndings = UNIX | PC | MAC deriving (Enum,Bounded)
 leMapping = listArray (0,2) ["unix","pc","mac"]
@@ -12,39 +12,44 @@ leMapping = listArray (0,2) ["unix","pc","mac"]
 data Security = PUBLIC  | PRIVATE | USEMASK deriving (Enum,Bounded)
 secMapping = listArray (0,2) ["public","private","usemask"]
 
-data Property = Property { name, value :: ByteString }
+data Property = Property { name, value :: String }
 
-data Event = Event { event, subject :: Maybe BStr.ByteString, 
+data Event = Event { event, subject :: Maybe String, 
                      lineendings :: LineEndings,
                      security :: Maybe Security,
                      allowmask :: Word32,
-                     year, mon, day, hour, minute :: Int,
+                     date :: DateTime,
                      metadata :: Maybe [Property] }
 
-type Entry2PairTransformer = Event -> Maybe Pair
+type Entry2PairTransformer = Event -> [Maybe Pair]
+
 transformers :: [Entry2PairTransformer]
-transformers = [ mbEventPair "event" event,
-                 mbEventPair "subject" subject,
-                 trLineendings,
-                 mbSecurity,
-                 makeShowableEventPair "allowmask" allowmask,
-                 makeShowableEventPair "year" year,
-                 makeShowableEventPair "mon" mon,
-                 makeShowableEventPair "day" day,
-                 makeShowableEventPair "hour" hour,
-                 makeShowableEventPair "min" minute
+transformers = [ mbEventPair "event" event ,
+                 mbEventPair "subject" subject ,
+                 trLineendings ,
+                 mbSecurity ,
+                 makeShowableEventPair "allowmask" allowmask ,
                  --TODO implement parsing of metadata here
-                 ]
+                 datePairs ]
     where
-        trLineendings = Just . makePair "lineendings" . enum2String leMapping . lineendings
-        mbSecurity = fmap ( makePair "security" . enum2String secMapping) . security
-        enum2String arr = (arr !) . fromEnum
+        trLineendings = (:[]) . Just . makePair "lineendings" . enumToString leMapping . lineendings
+        mbSecurity = (:[]) . fmap ( makePair "security" . enumToString secMapping) . security
+        enumToString arr = (arr !) . fromEnum
 
-mbEventPair :: String -> (Event -> Maybe BStr.ByteString) -> Event -> Maybe Pair
-mbEventPair name f post = fmap (makePairBSValue name) $ f post
+mbEventPair :: String -> (Event -> Maybe String) -> Event -> [Maybe Pair]
+mbEventPair name f post = (:[]) . fmap (makePair name) $ f post
 
-makeShowableEventPair :: (Show a) => String -> (Event -> a) -> Event -> Maybe Pair
-makeShowableEventPair name f = Just . makePair name . show . f
+makeShowableEventPair :: (Show a) => String -> (Event -> a) -> Event -> [Maybe Pair]
+makeShowableEventPair name f = (:[]) . Just . makePair name . show . f
+
+datePairs :: Event -> [ Maybe Pair]
+datePairs = toPairs . toGregorian . date
+    where
+        toPairs (year,month,day,hours,minutes,_) = fmap Just [ makePair "year" $ show year,
+                                                               makePair "mon" $ show month,
+                                                               makePair "day" $ show day,
+                                                               makePair "hour"$ show hours,
+                                                               makePair "min" $ show minutes ]
 
 event2pairs :: Event -> [Pair]
-event2pairs post = concatMap ( maybeToList . ( $post ) ) transformers
+event2pairs post = concatMap ( concatMap maybeToList . ( $post ) ) transformers
--- LiveJournal/Post/PostEvent.hs
+++ LiveJournal/Post/PostEvent.hs
@@ -6,14 +6,17 @@ import LiveJournal.Session
 import LiveJournal.Error
 import LiveJournal.Pair as P
 import Data.ByteString.Char8 as BStr
+import Data.Maybe
 
-postEntry :: Session -> String -> Event -> IO (Result (String))
-postEntry session username post = do
+postEntry :: Session -> String -> Maybe String -> Event -> IO (Result (String))
+postEntry session username target post = do
     makeLJCall session pairs extractEntryURL
     where
         pairs = P.makePair "mode" "postevent" : 
                 P.makePair "user" username : 
-                event2pairs post
+                event2pairs post ++ targetJr
+        targetJr | target == Nothing = []
+                 | otherwise = [P.makePair "usejournal" ( fromJust target )]
 
 extractEntryURL :: [Pair] -> Result String
 extractEntryURL  = extractEntryURL' . P.findPair "url"
