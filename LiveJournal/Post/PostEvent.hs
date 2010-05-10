3f8cbda535302027b457634f455e7d49dfe9b831
Eugene <bofh@redwerk.com>
2010-05-10 23:20:09 +0300

    Added post update method

--- LiveJournal/Post/PostEvent.hs
+++ LiveJournal/Post/PostEvent.hs
@@ -1,4 +1,7 @@
-module LiveJournal.Post.PostEvent where
+module LiveJournal.Post.PostEvent (
+    postEvent,
+    editEvent
+) where
 
 import LiveJournal.Post.Event
 import LiveJournal.Common
@@ -7,19 +10,33 @@ import LiveJournal.Error
 import LiveJournal.Pair as P
 import Data.ByteString.Char8 as BStr
 import Data.Maybe
+import Prelude as Pr
 
-postEntry :: Session -> String -> Maybe String -> Event -> IO (Result (String))
-postEntry session username target post = do
+postEvent :: Session -> String -> Maybe String -> Event -> IO (Result (String))
+postEvent session username target post = do
     makeLJCall session pairs extractEntryURL
     where
         pairs = P.makePair "mode" "postevent" : 
                 P.makePair "user" username : 
-                event2pairs post ++ targetJr
-        targetJr | target == Nothing = []
-                 | otherwise = [P.makePair "usejournal" ( fromJust target )]
+                createEvent post target
+
+editEvent :: Session -> String -> Maybe String -> String -> Event -> IO (Result (String))
+editEvent session username target eventid post = do
+    makeLJCall session pairs extractEntryURL
+    where
+        pairs = P.makePair "mode" "editevent" :
+                P.makePair "user" username :
+                P.makePair "eventid" eventid :
+                createEvent post target
 
 extractEntryURL :: [Pair] -> Result String
 extractEntryURL  = extractEntryURL' . P.findPair "url"
     where
         extractEntryURL' Nothing = Left WrongResponseFormat
         extractEntryURL' (Just url) = Right . BStr.unpack $ url
+
+targetJr :: Maybe String -> [Pair]
+targetJr = maybeToList . fmap (P.makePair "usejournal")
+
+createEvent :: Event -> Maybe String -> [Pair]
+createEvent post target = Pr.concat [event2pairs post, targetJr target ]
